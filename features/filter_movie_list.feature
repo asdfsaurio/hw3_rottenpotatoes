8ebf7cb78a0768a5934090f297d7c4a5afe0c821
saasbook <saasbook@saasbook.(none)>
2013-08-11 13:22:21 -0700

    part 2 + 3

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,27 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: PG-13, NC-17, G
   # enter step to "submit" the search form on the homepage
+  And I press "ratings_submit"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see movies with ratings: PG, R
   # enter step(s) to ensure that other movies are not visible
+  And I should not see movies with ratings: PG-13, NC-17, G
 
 Scenario: no ratings selected
   # see assignment
+#When I uncheck all the ratings
+  When I uncheck the following ratings: PG, R, PG-13, NC-17, G
+#Then I should see no movies
+  Then I should see all of the movies
 
 Scenario: all ratings selected
   # see assignment
+#When I check all the ratings
+  When I check the following ratings: PG, R, PG-13, NC-17, G
+#Then I should see every movie
+  Then I should see all of the movies
+
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,22 +1,27 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    pelicula = Movie.create
-    pelicula.title        = movie[:title]
-    pelicula.rating       = movie[:rating]
-    pelicula.release_date = movie[:release_date]
+#    if Movie.where("title = ?",movie[:title]) == nil
+#    then
+       pelicula = Movie.create
+       pelicula.title        = movie[:title]
+       pelicula.rating       = movie[:rating]
+       pelicula.release_date = movie[:release_date]
+       pelicula.save
 
-    pelicula.save!
+#pelicula.save!
+#    end
     # debugger
 
 
-    pelicula = Movie.where("title = ?",movie[:title])
-    if Movie.where("title = ?",movie[:title]) == nil then flunk "CACA" end
+#if Movie.where("title = ?",movie[:title]) == nil then flunk "CACA" end
 
   end
+  Movie.all.count.should == 10
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -28,6 +33,16 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
+
+
+#When /^(?:|I )check "([^"]*)"$/ do |field|
+#  check(field)
+#end
+#
+#When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+#  uncheck(field)
+#end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
@@ -36,5 +51,73 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-#debugger
+  if uncheck == nil
+  then
+    rating_list.split(",").each do |unitem|
+       step %Q{I check "ratings_#{unitem.strip}"}
+    end
+  else
+    rating_list.split(",").each do |unitem|
+       step %Q{I uncheck "ratings_#{unitem.strip}"}
+    end
+  end
+
 end
+
+
+Then /I should( not)? see movies with ratings: (.*)/ do |negative,rating_list|
+  # enter step(s) to ensure that other movies are not visible
+  rowcount=0
+  if negative == nil
+  then
+
+    rating_list.split(",").each do |unitem|
+#step %Q{I should see "#{unitem.strip}"}
+#step %Q{the "tbody" field should contain "#{unitem.strip}"}
+#    page.all('tbody#moviesbody').
+     rowcount += Movie.where("rating = ?", unitem.strip).count
+    end
+
+    rowcount.should == page.all('tbody/tr').count #/ 2
+
+  else
+    rating_list.split(",").each do |unitem|
+     rowcount += Movie.where("rating = ?", unitem.strip).count
+
+#rating_list.split(",").each do |unitem|
+#step %Q{I should not see "#{unitem.strip}"}
+#step %Q{the "moviesbody" field should not contain "#{unitem.strip}"}
+    end
+    
+    (Movie.all.count - rowcount).should == page.all('tbody/tr').count #/ 2
+
+  end
+end
+#  And I should not see movies with ratings PG-13, NC-17, G
+
+
+#Then /I should see no movies/ do 
+#  if false #asdf1 == "every"
+#  then
+#    Movie.all.count.should == page.all('tbody/tr').count
+#  else
+#    page.all('tbody/tr').count.should == 0
+#  end
+#end
+
+
+Then /I should see all of the movies/ do
+#debugger
+     Movie.all.count.should == page.all('tbody/tr').count
+end
+
+
+
+
+
+
+Then /I should see '(.*)' before '(.*)'/ do |title1,title2|
+#page.body.matches(/.*#{title1}.*#{title2}.*/)
+  page.body.index(title1).should < page.body.index(title2)
+end
+
